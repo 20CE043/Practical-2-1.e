5ca61921798d9f5ab5265fcaf81777ccc01dec2c
20CE043 <97497245+20CE043@users.noreply.github.com>
2022-01-11 10:48:33 +0530

    Add files via upload

---
+++ pratical2_1_e.py
@@ -0,0 +1,16 @@
+#Name: Hena Kharwa
+#ID: 20CE043
+#Github link: https://github.com/20CE043/Practical-2-1.e.git
+#Aim: Write a Python script to concatenate following dictionaries to create a new one.
+dic1={1:10,2:20}
+dic2={3:30,4:40}
+dic3={5:50,6:60}
+dic4={}
+#concating dic4 and dic1
+dic4.update(dic1)
+#concating dic4 and dic2
+dic4.update(dic2)
+#concating dic4 and dic3
+dic4.update(dic3)
+#Printing updated dictionary
+print (dic4)
